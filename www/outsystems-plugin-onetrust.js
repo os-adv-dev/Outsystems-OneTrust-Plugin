ac28ea8ef47714d59266a71db9cbd74f481824ee
Diogo Martins <diogo.martins@outsystems.com>
2024-05-10 11:49:57 +0100

    changes

--- www/outsystems-plugin-onetrust.js
+++ www/outsystems-plugin-onetrust.js
@@ -1,10 +1,5 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Plugin1_Extended', 'coolMethod', [arg0]);
+exports.onCreate = function (arg0, success, error) {
+	exec(success, error, "OutsystemsPluginOneTrust", "onCreate", [arg0]);
 };
-
-exports.coolMethodKotlin = function (arg0, success, error) {
-    exec(success, error, 'Plugin1Kotlin_Extended', 'coolMethodKotlin', [arg0]);
-};
-
