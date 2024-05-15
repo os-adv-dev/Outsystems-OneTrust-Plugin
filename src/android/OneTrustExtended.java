13032273ce7e8c968475c6745fa9296d4bd9aede
Diogo Martins <diogo.martins@outsystems.com>
2024-05-15 17:30:45 +0100

    Fixed missing import

--- src/android/OneTrustExtended.java
+++ src/android/OneTrustExtended.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPreferences;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
